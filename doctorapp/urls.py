8ec11ab191502ce55f3cb0110eb251da177295b3
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-09-29 08:48:32 -0600

    Add url to list_patients and change name file drhousecastdatates.json to housemdcastdatates.json

--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+from patients.views import list_patients
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/patients/', list_patients),
 ]
