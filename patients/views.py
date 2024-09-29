3d7a8703085059fa9993b06ff1d7871486176c50
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-09-29 09:01:29 -0600

    Improving list_patients view

--- patients/views.py
+++ patients/views.py
@@ -3,8 +3,15 @@ from .models import Patient
 from rest_framework.decorators import api_view 
 from rest_framework.response import Response
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def list_patients(request):
-    patients = Patient.objects.all()
-    serializer = PatientSerializer(patients, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = PatientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
