d66454f1ec7ea14e7eecdebe812490fca16f1808
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-09-29 08:27:52 -0600

    Add list_patients function in patients/view.py

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .serializers import PatientSerializer
+from .models import Patient
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def list_patients(request):
+    patients = Patient.objects.all()
+    serializer = PatientSerializer(patients, many=True)
+    return Response(serializer.data)
