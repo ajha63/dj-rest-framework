b0816955e173e753004c4b53ccab8aee0f3711be
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-09-29 14:42:09 -0600

    Added doctors view adn functions to list, create, modify and delete

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from .serializers import DoctorSerializer
+from .models import Doctor
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+# GET  /api/patients > List
+# POST /api/patients > Create/Add
+# GET  /api/patients/pk/ > one patient details
+# POST /api/patients/pk/ > one patient modify
+# DELETE /api/patients/pk/ > one patient modify
+
+@api_view(['GET', 'POST'])
+def list_doctors(request):
+    if request.method == 'GET':
+        doctors = Doctor.objects.all()
+        serializer = DoctorSerializer(doctors, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = DoctorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detail_doctor(request, pk):
+    try:
+        doctor = Doctor.objects.get(id=pk)
+    except Doctor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DoctorSerializer(doctor)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = DoctorSerializer(doctor, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    if request.method == 'DELETE':
+        doctor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
