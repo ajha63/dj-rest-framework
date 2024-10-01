93278d400b668c395fa6313a37ee0de08d7d9b6c
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-10-01 12:54:49 -0600

    Refactor DetailPatientView with Generic views

--- patients/views.py
+++ patients/views.py
@@ -4,7 +4,7 @@ from .models import Patient
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 # GET  /api/patients > List
@@ -20,34 +20,8 @@ class ListPatientsView(ListAPIView,CreateAPIView):
     queryset = Patient.objects.all()
 
 
-class DetailPatientView(APIView):
+class DetailPatientView(RetrieveUpdateDestroyAPIView):
     allowed_methods = ['GET', 'PUT', 'DELETE']
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
 
-    def get(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = PatientSerializer(patient)
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = PatientSerializer(patient, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    def delete(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        patient.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
