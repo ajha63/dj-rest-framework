cdfd0f90ae4779c44371880d4ac853815af498fc
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-10-05 16:37:31 -0600

    How to use SerializerMethodField in Django REST Framework

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,3 +1,4 @@
+from datetime import date
 from rest_framework import serializers
 
 from .models import Patient, Insurance, MedicalRecord
@@ -5,6 +6,7 @@ from bookings.serializers import AppointmentSerializer
 
 class PatientSerializer(serializers.ModelSerializer):
     appointments = AppointmentSerializer(many=True, read_only=True)
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Patient
@@ -12,6 +14,7 @@ class PatientSerializer(serializers.ModelSerializer):
             'id',
             'first_name',
             'last_name',
+            'age',
             'date_of_birth',
             'contact_number',
             'email',
@@ -19,6 +22,16 @@ class PatientSerializer(serializers.ModelSerializer):
             'medical_history',
             'appointments',
         ]
+
+    def get_age(self, obj):
+        """
+        Return the patient age in years
+        """
+
+        # get age in days (timedelta)
+        age_timedelta = date.today() - obj.date_of_birth
+        years = age_timedelta.days // 365
+        return f"{years}"
 
 class InsuranceSerializer(serializers.ModelSerializer):
     class Meta:
