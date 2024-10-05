77f39e0b868941ee608618252bdcb83ea7e6cb83
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-10-05 16:42:48 -0600

    Recover doctors/serializers.py code

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -2,16 +2,17 @@ from rest_framework import serializers
 
 from .models import Doctor, Department, DoctorAvailability, MedicalNote
 
+
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = '__all__'
 
     def validate_email(self, value):
-        if "@plainsboroteaching.care" in value:
+        if "@example.com" in value:
             return value
-        raise serializers.ValidationError("Permitidos solo correos del dominio @plainsboroteaching.care")
-    
+        raise serializers.ValidationError("El correo debe incluir @example.com")
+
     def validate(self, attrs):
         if len(attrs['contact_number']) < 10 and attrs['is_on_vacation']:
             raise serializers.ValidationError(
@@ -25,13 +26,14 @@ class DepartmentSerializer(serializers.ModelSerializer):
         model = Department
         fields = '__all__'
 
+
 class DoctorAvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
         model = DoctorAvailability
         fields = '__all__'
 
+
 class MedicalNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalNote
         fields = '__all__'
-
