adb0d6490eeeb00a78257ed6baf55c998bb4bb8d
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2024-10-06 15:46:37 -0600

    Adding Nested endpoint for Appointments using @action

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from bookings.serializers import AppointmentSerializer
+from bookings.models import Appointment
 from .serializers import DoctorSerializer
 from .models import Doctor
 from .permissions import IsDoctor
@@ -13,15 +15,32 @@ class DoctorViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsDoctor]
 
     @action(['POST'], detail=True, url_path='set-on-vacation')
-    def set_on_vacation(self, requests, pk):
+    def set_on_vacation(self, request, pk):
         doctor = self.get_object()
         doctor.is_on_vacation = True
         doctor.save()
         return Response({"status": "El doctor está en vacaciones"})
 
     @action(['POST'], detail=True, url_path='set-off-vacation')
-    def set_off_vacation(self, requests, pk):
+    def set_off_vacation(self, request, pk):
         doctor = self.get_object()
         doctor.is_on_vacation = False
         doctor.save()
         return Response({"status": "El doctor NO está en vacaciones"})
+
+    @action(['POST', 'GET'], detail=True, serializer_class=AppointmentSerializer)
+    def appointments(self, request, pk):
+        doctor = self.get_object()
+
+        if request.method == 'POST':
+            data = request.data.copy()
+            data['doctor'] = doctor.id
+            serializer = AppointmentSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'GET':
+            appointments = Appointment.objects.filter(doctor=doctor)
+            serializer = AppointmentSerializer(appointments, many=True)
+            return Response(serializer.data)
